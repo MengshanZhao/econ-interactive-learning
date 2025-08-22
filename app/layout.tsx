9e5a6cbf0643b91adc1c498a9bdd24d37259adbd
Mengshan Zhao <72824905+MengshanZhao@users.noreply.github.com>
2025-08-22 14:07:12 -0700

    traffic check

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css"
 import Navbar from "@/components/navbar"
 import Footer from "@/components/footer"
 import { ThemeProvider } from "@/components/theme-provider"
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] })
 
